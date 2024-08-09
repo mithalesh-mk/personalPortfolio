512b7d2f9fd76df6a31f565de1529cdaa233cf02
Mithalesh Kumar Bind <mk2018me@gmail.com>
2024-08-10 00:40:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './Components/NavBar';
+import Banner from './Components/Banner';
+import Skiils from './Components/Skiils';
+import DemoComp from './Components/DemoComp';
+import MyTabs from './Components/MyTabs';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
+
+
+
+
+function App() {
+  
+  return ( 
+    <>
+    <NavBar />
+    <Banner />
+    <Skiils />
+    <MyTabs />
+
+    <Contact />
+    <Footer />
+    
+  
+    </>
+    
+  )
+}
+
+export default App
---
+++ src/Components/DemoComp.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import dsa  from '../assets/dsa.png'
+import ml from '../assets/machineLearning.png'
+import web from '../assets/webD.svg'
+import py from '../assets/python.png'
+
+
+function DemoComp() {
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+  return (
+    <div className="slider-container">
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+        <div>
+          <h3>7</h3>
+        </div>
+        <div>
+          <h3>8</h3>
+        </div>
+      </Slider>
+    </div>
+    );
+}
+
+export default DemoComp;
